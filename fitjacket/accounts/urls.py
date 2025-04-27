e2d7a8c78e228e1e62023b533c862970adcbdb4e
nimamollaei6 <nimamollaei06@gmail.com>
2025-04-27 13:42:43 -0400

    added all friends system features

--- fitjacket/accounts/urls.py
+++ fitjacket/accounts/urls.py
@@ -9,10 +9,10 @@ from django.contrib.auth import views as auth_views
 app_name = "accounts"
 
 urlpatterns = [
-    path('signup/', views.signup, name='accounts.signup'),
-    path('login/', views.login, name='accounts.login'),
-    path('logout/', views.logout, name='accounts.logout'),
-    path('password-reset/', ResetPasswordView.as_view(), name='accounts.password_reset'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
          name='password_reset_confirm'),
--- fitjacket/accounts/views.py
+++ fitjacket/accounts/views.py
@@ -12,6 +12,10 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
+from friend.utils import get_friend_request_or_false
+from friend.models import FriendList
+from friend.models import FriendRequest
+from friend.friend_request_status import FriendRequestStatus
 
 def signup(request):
     template_data = {}
@@ -23,7 +27,7 @@ def signup(request):
         form = CustomUserCreationForm(request.POST, error_class = CustomErrorList)
         if form.is_valid():
             form.save()
-            return redirect('accounts.login')
+            return redirect('login')
         else:
             template_data['form'] = form
             return render(request, 'accounts/signup.html', {'template_data': template_data})
@@ -74,24 +78,71 @@ def account_view(request, *args, **kwargs):
     except User.DoesNotExist:
         return HttpResponse("That user doesn't exist.")
     if account:
-        context['id'] = account.is_authenticated
+        context['id'] = account.id
         context['username'] = account.username
         context['email'] = account.email
-        context['profile_image'] = account.profile_image.url
-        context['hide_email'] = account.hide_email
+
+        try:
+            friend_list = FriendList.objects.get(user=account)
+        except FriendList.DoesNotExist:
+            friend_list = FriendList(user=account)
+            friend_list.save()
+        friends = friend_list.friends.all()
+        context['friends'] = friends
 
         #Define state template variables
 
         is_self = True
         is_friend = False
+        request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
+        friend_requests = None
         user = request.user
         if user.is_authenticated and user != account:
             is_self = False
+            if friends.filter(pk=user.id):
+                is_friend=True
+            else:
+                is_friend=False
+                if get_friend_request_or_false(sender=account, receiver=user) != False:
+                    request_sent = FriendRequestStatus.THEM_SENT_TO_YOU.value
+                    context['pending_friend_request_id'] = get_friend_request_or_false(sender=account, receiver=user).id
+                elif get_friend_request_or_false(sender=user, receiver=account) != False:
+                    request_sent = FriendRequestStatus.YOU_SENT_TO_THEM.value
+                else:
+                    request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
         elif not user.is_authenticated:
             is_self = False
+        else:
+            try:
+                friend_requests = FriendRequest.objects.filter(receiver=user, is_active=True)
+            except:
+                pass
         
         context['is_self'] = is_self
         context['is_friend'] = is_friend
         context['BASE_URL'] = settings.BASE_URL
+        context['request_sent'] = request_sent
+        context['friend_requests'] = friend_requests
 
-        return render(request, "accounts/account.html")
+        return render(request, "accounts/account.html", context)
+    
+def account_search_view(request, *args, **kwargs):
+    context = {}
+
+    if request.method == "GET":
+        search_query = request.GET.get("q")
+        if len(search_query) > 0:
+            search_results = User.objects.filter(email__icontains=search_query).filter(username__icontains=search_query).distinct()
+            user = request.user
+            accounts = []
+            if user.is_authenticated:
+                auth_user_friend_list = FriendList.objects.get(user=user)
+                for account in search_results:
+                    accounts.append((account, auth_user_friend_list.is_mutual_friend(account)))
+                context['accounts'] = accounts
+            else:
+                for account in search_results:
+                    accounts.append((account, False))
+                context['accounts'] = accounts
+
+    return render(request, "accounts/search_results.html", context)
